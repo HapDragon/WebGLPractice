9f6620e7efa28a92fc7dd8d6fdabe5d366c3ff17
王秋艳 <wqy224488@163.com>
2020-03-27 08:30:18 +0800

    立方体部分代码提交

--- webgl/HelloCubes.js
+++ webgl/HelloCubes.js
@@ -22,6 +22,36 @@ function main() {
 		console.log('Failed to get the rendering context for webgl');
 		return;
 	}
+	gl.clearColor(0,0,0,1);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	if(!initShaders(gl,VSHADER_SOURCE,FSHADER_SOURCE)){
+		console.log('Failed to initialize shader.');
+		return;
+	}
+	//设置顶点坐标和颜色
+	var n=initVertexBuffers(gl);
+	if(n<0)
+	{
+		console.log("Failed to set the positions of the vertices");
+		return;
+	}
+
+	var u_mvpMatrix=gl.getUniformLocation(gl.program,'u_mvpMatrix');
+	if(!u_mvpMatrix){
+		console.log('Failed to get uniform position of u_mvpMatrix.');
+		return;
+	}
+	gl.enable(gl.DEPTH_TEST);//开启深度测试
+	gl.clear(gl.DEPTH_BUFFER_BIT);//清空深度缓冲区
+	gl.enable(gl.POLYGON_OFFSET_FILL);
+	//设置视点和可视空间
+	var mvpMatrix=new Matrix4();
+	mvpMatrix.setPerspective(30,1,1,100);
+	mvpMatrix.lookAt(3,3,7,0,0,0,0,1,0);
+	//将模型视图投影矩阵传给u_MvpMatrix
+	gl.uniformMatrix4fv(u_mvpMatrix,false,mvpMatrix.elements);
+	gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+	gl.drawElements(gl.TRIANGLES,n,gl.UNSIGNED_BYTE,0);
 }
 
 function initVertexBuffers(gl) {
