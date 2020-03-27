656b050b08e67911a812fc8b3133ce73bf80e30e
王秋艳 <wqy224488@163.com>
2020-03-27 14:21:19 +0800

    逐顶点渲染立方体提交

--- webgl/HelloCubes.js
+++ webgl/HelloCubes.js
@@ -59,8 +59,60 @@ function initVertexBuffers(gl) {
         //顶点坐标和颜色
          1.0, 1.0, 1.0,1.0,1.0,1.0,//v0 白色
         -1.0, 1.0, 1.0,1.0,0.0,1.0,//v1 品红色
-        -1.5,-1.0, 1.0,1.0,0.0,0.0,//v2 红色
-
-
+        -1.0,-1.0, 1.0,1.0,0.0,0.0,//v2 红色
+		1.0, -1.0, 1.0,1.0,1.0,0.0,//v3 黄色
+		1.0, -1.0, -1.0,0.0,1.0,0.0,//v4 绿色
+		1.0,1.0, -1.0,0.0,1.0,1.0,//v5 青色
+		-1.0, 1.0, -1.0,0.0,0.0,1.0,//v6 蓝色
+		-1.0,-1.0, -1.0,0.0,0.0,0.0,//v7 黑色
     ]);
+    var indices=new Uint8Array([
+    	0,1,2,0,2,3,//前
+		0,3,4,0,4,5,//右
+		0,1,6,0,5,6,//上
+		1,6,7,1,2,7,//左
+		2,7,4,2,4,3,//下
+		5,6,7,5,7,4//后
+	]);
+    //创建缓冲区对象
+	var vertexColorBuffer=gl.createBuffer();
+	if(!vertexColorBuffer){
+		console.log('Failed to create the buffer object ');
+		return -1;
+	}
+	var indexBuffer=gl.createBuffer();
+	if(!indexBuffer){
+		console.log('Failed to create the buffer object ');
+		return -1;
+	}
+	//将缓冲区对象绑定到目标
+	gl.bindBuffer(gl.ARRAY_BUFFER,vertexColorBuffer);
+	//向缓冲区对象写入数据
+	gl.bufferData(gl.ARRAY_BUFFER,verticesColors,gl.STATIC_DRAW);
+	var FSIZE=verticesColors.BYTES_PER_ELEMENT;
+	var a_Position=gl.getAttribLocation(gl.program,'a_Position');
+	if(a_Position<0){
+		console.log('Failed to get the storage location of a_Position');
+		return -1;
+	}
+	//将缓冲区对象分配给a_Position变量
+	gl.vertexAttribPointer(a_Position,3,gl.FLOAT,false,FSIZE*6,0);
+	//开启attribute变量：连接a_Position变量与分配给它的缓冲区对象
+	gl.enableVertexAttribArray(a_Position);
+
+	var a_Color=gl.getAttribLocation(gl.program,'a_Color');
+	if(a_Color<0){
+		console.log('Failed to get the storage location of a_Color');
+		return -1;
+	}
+	//将缓冲区对象分配给a_Position变量
+	gl.vertexAttribPointer(a_Color,3,gl.FLOAT,false,FSIZE*6,FSIZE*3);
+	//开启attribute变量：连接a_Position变量与分配给它的缓冲区对象
+	gl.enableVertexAttribArray(a_Color);
+
+	//将顶点索引数据写入缓冲区对象
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,indexBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,indices,gl.STATIC_DRAW);
+
+	return indices.length;
 }
